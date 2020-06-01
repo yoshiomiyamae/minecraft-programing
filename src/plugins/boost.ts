15634d4d8ef7b699538782ce10e4e846b4976262
Yoshio Miyamae <yoshio.miyamae@famoce-succellion.com>
2020-06-01 22:17:29 +0900

    操作がしにくいので修正

--- src/plugins/boost.ts
+++ src/plugins/boost.ts
@@ -5,15 +5,14 @@ import { CommandAndArgs, TargetSelector, Effect } from '../type';
 export const run = async (socket: ws, args: CommandAndArgs) => {
   socket.send(Command.effect(TargetSelector.Self, Effect.Clear));
   socket.send(Command.effect(TargetSelector.Self, Effect.Speed, 1000000, 8));
-  socket.send(Command.effect(TargetSelector.Self, Effect.Haste, 1000000, 8));
+  socket.send(Command.effect(TargetSelector.Self, Effect.Haste, 1000000, 1));
   socket.send(Command.effect(TargetSelector.Self, Effect.Strength, 1000000, 8));
   socket.send(Command.effect(TargetSelector.Self, Effect.InstantHealth, 1000000, 8));
-  socket.send(Command.effect(TargetSelector.Self, Effect.JumpBoost, 1000000, 8));
+  socket.send(Command.effect(TargetSelector.Self, Effect.JumpBoost, 1000000, 2));
   socket.send(Command.effect(TargetSelector.Self, Effect.Regeneration, 1000000, 8));
   socket.send(Command.effect(TargetSelector.Self, Effect.Resistance, 1000000, 8));
   socket.send(Command.effect(TargetSelector.Self, Effect.FireResistance, 1000000, 8));
   socket.send(Command.effect(TargetSelector.Self, Effect.WaterBreathing, 1000000, 8));
-  socket.send(Command.effect(TargetSelector.Self, Effect.NightVision, 1000000, 8));
   socket.send(Command.effect(TargetSelector.Self, Effect.NightVision, 1000000, 8));
   socket.send(Command.effect(TargetSelector.Self, Effect.HealthBoost, 1000000, 8));
   socket.send(Command.effect(TargetSelector.Self, Effect.Absorption, 1000000, 8));
